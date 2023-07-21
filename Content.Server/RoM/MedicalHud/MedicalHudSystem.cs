131c3ff60da40f6fec6cd012d6ca8adbdf8d1096
Remaster <borov704@gmail.com>
2023-07-21 15:09:31 +0200

    Fix problems

--- Content.Server/RoM/MedicalHud/MedicalHudSystem.cs
+++ Content.Server/RoM/MedicalHud/MedicalHudSystem.cs
@@ -7,6 +7,23 @@ namespace Content.Server.RoM.MedicalHud;
 [UsedImplicitly]
 public sealed class MedicalHudSystem : SharedMedicalHudSystem
 {
+    [Dependency] private readonly EntityManager _entityManager = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<MedicalHudComponent, GotEquippedEvent>(OnEquipped);
+        SubscribeLocalEvent<MedicalHudComponent, GotUnequippedEvent>(OnUnequipped);
+    }
 
+    private void OnUnequipped(EntityUid uid, MedicalHudComponent component, GotUnequippedEvent args)
+    {
+        if (HasComp<MedicalHudComponent>(args.Equipee))
+            _entityManager.RemoveComponent<MedicalHudComponent>(args.Equipee);
+    }
+
+    private void OnEquipped(EntityUid uid, MedicalHudComponent comp, GotEquippedEvent args)
+    {
+        _entityManager.AddComponent<MedicalHudComponent>(args.Equipee);
+    }
 }
 
--- Content.Shared/RoM/MedicalHud/SharedHudSystem.cs
+++ Content.Shared/RoM/MedicalHud/SharedHudSystem.cs
@@ -6,23 +6,6 @@ namespace Content.Shared.RoM.MedicalHud;
 [UsedImplicitly]
 public abstract class SharedMedicalHudSystem : EntitySystem
 {
-    [Dependency] private readonly EntityManager _entityManager = default!;
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<MedicalHudComponent, GotEquippedEvent>(OnEquipped);
-        SubscribeLocalEvent<MedicalHudComponent, GotUnequippedEvent>(OnUnequipped);
-    }
 
-    private void OnUnequipped(EntityUid uid, MedicalHudComponent component, GotUnequippedEvent args)
-    {
-        if (HasComp<MedicalHudComponent>(args.Equipee))
-            _entityManager.RemoveComponent<MedicalHudComponent>(args.Equipee);
-    }
-
-    private void OnEquipped(EntityUid uid, MedicalHudComponent comp, GotEquippedEvent args)
-    {
-        _entityManager.AddComponent<MedicalHudComponent>(args.Equipee);
-    }
 }
 
